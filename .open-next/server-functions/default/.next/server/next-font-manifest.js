e3ce14754478d89598396201af92c631a9d7e9e5
Hemant Sharma <hemant@hemantsharma.tech>
2025-05-27 20:57:56 +0530

    remove: .open-next/ dir

--- .open-next/server-functions/default/.next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/Users/hemantsharma/Developer/Projects/Ambitious Projects/mala-app/src/app/layout":["static/media/569ce4b8f30dc480-s.p.woff2","static/media/93f479601ee12b01-s.p.woff2","static/media/1059e7280a9af611-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
